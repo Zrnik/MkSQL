af29e93d240280154c3adb49dbd641150fe05674
Programátor <Programtor@DESKTOP-26S234P>
2020-08-03 12:16:25 +0200

    you can have keywords in strings or table names. they are in quotes anyway

--- src/Utils.php
+++ src/Utils.php
@@ -21,21 +21,6 @@ class Utils
         "0","1","2","3","4","5","6","7","8","9","_"
     ];
 
-    private static $SQLKeywords = [
-        "ADD", "ADD CONSTRAINT", "ALTER", "ALTER COLUMN", "ALTER TABLE", "ALL", "AND",
-        "ANY", "AS", "ASC", "BACKUP DATABASE", "BETWEEN", "CASE", "CHECK", "COLUMN",
-        "CONSTRAINT", "CREATE", "CREATE DATABASE", "CREATE INDEX", "CREATE OR REPLACE VIEW",
-        "CREATE TABLE", "CREATE PROCEDURE", "CREATE UNIQUE INDEX", "CREATE VIEW", "DATABASE",
-        "DEFAULT", "DELETE", "DESC", "DISTINCT", "DROP", "DROP COLUMN", "DROP CONSTRAINT",
-        "DROP DATABASE", "DROP DEFAULT", "DROP INDEX", "DROP TABLE", "DROP VIEW", "EXEC",
-        "EXISTS", "FOREIGN KEY", "FROM", "FULL OUTER JOIN", "GROUP BY", "HAVING", "IN",
-        "INDEX", "INNER JOIN", "INSERT INTO", "IS NULL", "IS NOT NULL",
-        "JOIN", "LEFT JOIN", "LIKE", "LIMIT", "NOT", "NOT NULL", "OR", "ORDER BY", "OUTER JOIN",
-        "PRIMARY KEY", "PROCEDURE", "RIGHT JOIN", "ROWNUM", "SELECT", "SELECT DISTINCT",
-        "SELECT INTO", "SELECT TOP", "SET", "TABLE", "TOP", "TRUNCATE TABLE", "UNION",
-        "UNION ALL", "UNIQUE", "UPDATE", "VALUES", "VIEW", "WHERE",
-    ];
-
     /**
      * @param $name
      * @param array $AdditionalAllowed
@@ -52,9 +37,6 @@ class Utils
 
         if(trim(str_replace($Allowed,"",$name)) !== "")
             throw new InvalidArgumentException("Argument '".$name."' contains invalid characters!");
-
-        if(str_replace(self::$SQLKeywords, "", strtoupper($name)) !== strtoupper($name))
-            throw new InvalidArgumentException("Argument '".$name."' contains SQL keyword!");
 
         return $name;
     }
