8e0dc82b08c72da59b719ec09802e3791a36bdb4
Štěpán Zrník <stepan.zrnik@gmail.com>
2021-11-12 14:05:12 +0100

    some hooks in Installable class

--- src/Utilities/Installable.php
+++ src/Utilities/Installable.php
@@ -72,7 +72,9 @@ abstract class Installable extends BaseRepository
 
         // Process Updated
         $this->updater->installable = null;
+        $this->beforeInstallation();
         $this->updater->install();
+        $this->afterInstallation();
     }
 
     public static function uninstallAll(PDO $pdo): void
@@ -102,5 +104,11 @@ abstract class Installable extends BaseRepository
     {
         return $this->updater->tableList();
     }
+
+    /** Hook fired before the 'install' function of the updater */
+    protected function beforeInstallation(): void { }
+
+    /** Hook fired after the 'install' function of the updater */
+    protected function afterInstallation(): void { }
 
 }
